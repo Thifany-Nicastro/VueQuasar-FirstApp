9961f99576d2638ea6b06a150980671e0348f1b9
Thifany Nicastro <thifanynicastro@hotmail.com>
2021-03-06 00:26:34 -0300

    Drawer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,97 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <q-list>
+        <q-item clickable v-ripple :to="{ name: 'Home' }">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+
+          <q-item-section>Home</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple :to="{ name: 'About' }">
+          <q-item-section avatar>
+            <q-icon name="info" />
+          </q-item-section>
+
+          <q-item-section>About</q-item-section>
+        </q-item>
+
+        <q-expansion-item
+          icon="inbox"
+          label="Inbox"
+          default-opened
+          :content-inset-level="1"
+        >
+            <q-item clickable v-ripple :to="{ name: 'Home' }">
+              <q-item-section avatar>
+                <q-icon name="mail" />
+              </q-item-section>
+
+              <q-item-section>Message</q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple :to="{ name: 'Home' }">
+              <q-item-section avatar>
+                <q-icon name="mail" />
+              </q-item-section>
+
+              <q-item-section>Message</q-item-section>
+            </q-item>
+
+            <!-- <q-item clickable v-ripple :to="{ name: 'About' }">
+              <q-item-section avatar class="q-ml-xl">
+                <q-icon color="primary" name="info" />
+              </q-item-section>
+
+              <q-item-section>About</q-item-section>
+            </q-item> -->
+        </q-expansion-item>
+        
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { nextTick, onMounted, ref } from 'vue'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="q-pa-md">
+    <div class="row justify-center">
+      <div class="col-6">
+        <q-card class="my-card">
+          <q-card-section>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et alias numquam odio aliquam explicabo eum quae, animi ipsa! Facilis aspernatur tempora assumenda, maiores optio error modi possimus quae ex tempore?
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
